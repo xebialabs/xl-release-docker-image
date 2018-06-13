c8f4fa053ce83f88e6700e591a1855f4a7fe3d17
Vincent Partington <vpartington@xebialabs.com>
2018-06-13 17:25:14 +0200

    Allow this default setup to be skipped by passing in -setup (and other parameters). Also copy plugins when conf directory is already there.

--- resources/bin/run-in-container.sh
+++ resources/bin/run-in-container.sh
@@ -1,34 +1,10 @@
 #!/bin/sh
 
-# Set up new installation
-if [ ! -f "${APP_HOME}/conf/xl-release-server.conf" ]; then
-  echo "Setting up new installation of XL Release"
-  echo "... Copying default configuration"
-  cd ${APP_HOME}/default-conf
-  for f in *; do
-    if [ -f ${APP_HOME}/conf/$f ]; then
-      echo "...... Not copying $f because it already exists in the conf directory"
-    else
-      cp -R $f ${APP_HOME}/conf/
-    fi
-  done
-  cd ${APP_HOME}
+# Copy default plugins
+if [ -z "$(ls -A ${APP_HOME}/plugins)" ]; then
+  echo "Empty ${APP_HOME}/plugins directory detected:"
+  echo "... Copying default plugins from ${APP_HOME}/default-plugins"
 
-  if [ "${ADMIN_PASSWORD}" == "" ]; then
-    ADMIN_PASSWORD=`pwgen 8 1`
-    echo "... Generated admin password: ${ADMIN_PASSWORD}"
-  fi
-  echo "admin.password=${ADMIN_PASSWORD}" >> ${APP_HOME}/conf/xl-release-server.conf
-
-  if [ "${REPOSITORY_KEYSTORE_PASSPHRASE}" == "" ]; then
-    REPOSITORY_KEYSTORE_PASSPHRASE=`pwgen 16 1`
-    echo "... Generated repository keystore passphrase: ${REPOSITORY_KEYSTORE_PASSPHRASE}"
-    echo "repository.keystore.password=${REPOSITORY_KEYSTORE_PASSPHRASE}" >> ${APP_HOME}/conf/xl-release-server.conf
-  fi
-  echo "... Generating repository keystore"
-  keytool -genseckey -alias deployit-passsword-key -keyalg aes -keysize 128 -keypass "deployit" -keystore ${APP_HOME}/conf/repository-keystore.jceks -storetype jceks -storepass ${REPOSITORY_KEYSTORE_PASSPHRASE}
-
-  echo "... Copying default plugins"
   cd ${APP_HOME}/default-plugins
   for pluginrepo in *; do
     if [ -d ${APP_HOME}/default-plugins/$pluginrepo ]; then
@@ -37,7 +13,7 @@ if [ ! -f "${APP_HOME}/conf/xl-release-server.conf" ]; then
       for pluginjar in *; do
         pluginbasename=${pluginjar%%-[0-9\.]*.jar}
         if [ -f ${APP_HOME}/plugins/*/$pluginbasename-[0-9\.]*.jar ]; then
-          echo "...... Not copying $pluginrepo/$pluginjar because a version of that plugin already exists in the plugins directory"
+          echo "... Not copying $pluginrepo/$pluginjar because a version of that plugin already exists in the plugins directory"
         else
           cp -R ${APP_HOME}/default-plugins/$pluginrepo/$pluginjar ${APP_HOME}/plugins/$pluginrepo/
         fi
@@ -50,6 +26,48 @@ if [ ! -f "${APP_HOME}/conf/xl-release-server.conf" ]; then
 
   echo "Done"
 fi
+
+# Set up new installation
+if [ ! -f "${APP_HOME}/conf/xl-release-server.conf" ]; then
+  echo "No ${APP_HOME}/conf/xl-release-server.conf file detected:"
+  echo "... Copying default configuration from ${APP_HOME}/default-conf"
+
+  cd ${APP_HOME}/default-conf
+  for f in *; do
+    if [ -f ${APP_HOME}/conf/$f ]; then
+      echo "... Not copying $f because it already exists in the conf directory"
+    else
+      cp -R $f ${APP_HOME}/conf/
+    fi
+  done
+  cd ${APP_HOME}
+
+  echo "Done"
+
+  if [ $# -eq 0 ]; then
+    echo "No arguments passed to container:"
+    echo "... Running default setup"
+
+    if [ "${ADMIN_PASSWORD}" == "" ]; then
+      ADMIN_PASSWORD=`pwgen 8 1`
+      echo "... Generating admin password: ${ADMIN_PASSWORD}"
+    fi
+
+    if [ "${REPOSITORY_KEYSTORE_PASSPHRASE}" == "" ]; then
+      REPOSITORY_KEYSTORE_PASSPHRASE=`pwgen 16 1`
+      echo "... Generating repository keystore passphrase: ${REPOSITORY_KEYSTORE_PASSPHRASE}"
+    fi
+    echo "... Generating repository keystore"
+    keytool -genseckey -alias deployit-passsword-key -keyalg aes -keysize 128 -keypass "deployit" -keystore ${APP_HOME}/conf/repository-keystore.jceks -storetype jceks -storepass ${REPOSITORY_KEYSTORE_PASSPHRASE}
+
+    echo "... Generating xl-release-server.conf"
+    sed -e "s/\${ADMIN_PASSWORD}/${ADMIN_PASSWORD}/g" -e "s/\${REPOSITORY_KEYSTORE_PASSPHRASE}/${REPOSITORY_KEYSTORE_PASSPHRASE}/g" ${APP_HOME}/conf/xl-release-server.conf.template > ${APP_HOME}/conf/xl-release-server.conf
+
+    echo "Done"
+  fi
+fi
+
+
 
 # Generate node specific configuration with IP address of container
 IP_ADDRESS=$(hostname -i)
