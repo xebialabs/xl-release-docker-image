ea23303258a75dcf110eaa90a558278ee7e8978b
Paul van der Ende <pvanderende@xebialabs.com>
2018-06-20 13:56:49 +0200

    added build script and removed 'v' from version

---
+++ build.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+####### PARSING OPTIONS ################################################################################################
+
+PUSH=false
+
+while getopts ":u:p:U:P:g:r:v:s:h" opt; do
+  case ${opt} in
+    u ) DL_USER=$OPTARG;;
+    p ) DL_PASS=$OPTARG;;
+    U ) REPOSITORY_USER=$OPTARG;;
+    P ) REPOSITORY_PASS=$OPTARG;;
+    g ) REGISTRY=$OPTARG;;
+    r ) REPOSITORY=$OPTARG;;
+    v ) VERSION=$OPTARG;;
+    h ) PUSH=true;;
+    s ) SOURCE=$OPTARG;;
+    \? ) echo "Invalid option: $OPTARG" 1>&2; exit 1;;
+    : ) echo "Invalid option: $OPTARG requires an argument" 1>&2; exit 1;;
+  esac
+done
+shift $((OPTIND -1))
+
+if [ -z ${REGISTRY+x} ] || [ -z ${REPOSITORY+x} ] || [ -z ${VERSION+x} ] ; then
+    echo "Options -v -r -g are mandatory"
+    exit 1
+fi
+
+######## DOWNLOADING DIST ##############################################################################################
+
+if [ -z ${DL_USER+x} ] || [ -z ${DL_PASS+x} ] || [ -z ${SOURCE+x} ]; then
+   echo "Skipping download because download user, pass or source are not set."
+else
+    DIST_DEST="resources/xl-release-${VERSION}-server.zip"
+    if [ -f $DIST_DEST ]; then
+        echo "Skipping download because file already exists"
+    else
+        DL_LOCATION_DIST="https://dist.xebialabs.com/customer/xl-release/product/${VERSION}/xl-release-${VERSION}-server.zip"
+        DL_LOCATION_NEXUS="https://nexus.xebialabs.com/nexus/service/local/repositories/releases/content/com/xebialabs/xlrelease/xl-release/${VERSION}/xl-release-${VERSION}-server.zip"
+        if [ "$SOURCE" = "nexus" ]; then
+            DL_LOCATION=$DL_LOCATION_NEXUS
+        else
+            DL_LOCATION=$DL_LOCATION_DIST
+        fi
+
+        echo "File does not exist, downloading from ${DL_LOCATION}..."
+        curl --fail -u "${DL_USER}:${DL_PASS}" "${DL_LOCATION}" > "${DIST_DEST}"
+    fi
+fi
+
+echo "Product zip located at: ${DIST_DEST}"
+
+docker build --build-arg XLR_VERSION="${VERSION}" --tag "${REGISTRY}/${REPOSITORY}:${VERSION}" -f alpine/Dockerfile .
+
+####### PUSH TO REGISTRY ###############################################################################################
+
+if  ! [ -z ${REPOSITORY_USER+x} ] && ! [ -z ${REPOSITORY_PASS+x} ] ; then
+    echo "Login in to registry with user ${REPOSITORY_USER}"
+    docker login -u "${REPOSITORY_USER}" -p "${REPOSITORY_PASS}"
+fi
+
+if [ $PUSH = true ]; then
+    echo "Pushing image to remote"
+    docker push "${REGISTRY}/${REPOSITORY}:${VERSION}"
+else
+    echo "Skipping push"
+fi
+
+
